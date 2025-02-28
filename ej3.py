aaa3e8661db2722258336df80ee23b9f6a107d7d
Cesarmb3 <146852522+Cesarmb3@users.noreply.github.com>
2025-02-28 22:00:11 +0100

    ej3 más sencillo

--- ej3.py
+++ ej3.py
@@ -1,6 +1,6 @@
 import re
 
-patron_fecha = re.compile(r"(\b\d{4})-(\d{2})-(\d{2}\b)")
+patron_fecha = re.compile(r"(\b[0-9]{4})-([0-9]{2})-([0-9]{2}\b)")
 texto = input().strip()
 resultado = patron_fecha.sub(lambda m: f"{m.group(3)}.{m.group(2)}.{m.group(1)}", texto)
 print(resultado)
